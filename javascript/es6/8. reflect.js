83798e44c67920adfef3a1c38e7169c3ca7b1c97
Aftab Quraishi <aftab.reg@gmail.com>
2018-04-18 00:06:55 +0600

    fixed rename of files

---
+++ javascript/es6/8. reflect.js
@@ -0,0 +1,15 @@
+// Reflect
+// ------------------------------------------------------
+
+// ---- Reflect.construct() ----
+
+// ---- Reflect.apply() ----
+
+// ---- Reflect access property ----
+
+// ---- Reflect.ownKeys() ----
+
+// ---- Reflect create & delete property ----
+
+// ---- Reflect prevent extend ----
+
