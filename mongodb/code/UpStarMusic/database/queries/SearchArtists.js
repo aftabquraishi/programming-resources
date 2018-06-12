0364bff9aa9c56e69b635583e5a6b4032e303639
Aftab Quraishi <aftab.reg@gmail.com>
2018-06-13 03:23:09 +0600

    search/filter queries

--- mongodb/code/UpStarMusic/database/queries/SearchArtists.js
+++ mongodb/code/UpStarMusic/database/queries/SearchArtists.js
@@ -9,4 +9,47 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+    const query = Artist
+        .find(buildQuery(criteria))
+        .sort({ [sortProperty]: 1 })
+        .skip(offset)
+        .limit(limit);
+    
+    const count = Artist
+        .find(buildQuery(criteria))
+        .count();
+
+    return Promise.all([ query, count ])
+        .then((results) => { 
+            return {
+                all: results[0], 
+                count: results[1], 
+                offset: offset, 
+                limit: limit 
+            };
+        });
 };
+
+const buildQuery = function(criteria) {
+    const query = {};
+    
+    if (criteria.name) {
+        query.$text = { $search: criteria.name };
+    }
+
+    if (criteria.age) {
+        query.age = {
+            $gte: criteria.age.min,
+            $lte: criteria.age.max
+        };
+    }
+
+    if (criteria.yearsActive) {
+        query.yearsActive = {
+            $gte: criteria.yearsActive.min,
+            $lte: criteria.yearsActive.max
+        };
+    }
+    
+    return query;
+}
